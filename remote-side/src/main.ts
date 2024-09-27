b24b44a119fa05ad8cabaaadc4463821e5569559
Seif Eddine Ben Hmida <seifeddine.benhmida@minotore.com>
2024-09-27 16:44:14 +0100

    Add zoneless to remote

--- remote-side/src/main.ts
+++ remote-side/src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 
 import { AppComponent } from "./app";
+import { provideExperimentalZonelessChangeDetection } from "@angular/core";
 
 bootstrapApplication(AppComponent, {
-  providers: [],
+  providers: [provideExperimentalZonelessChangeDetection()],
 });
