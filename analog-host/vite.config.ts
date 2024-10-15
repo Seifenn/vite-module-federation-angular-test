7bd7cf79cc6f405d8b96a6e5de6fdbfc2dce8c4c
Seifenn <seifeddine.benhmida@minotore.com>
2024-10-15 11:41:12 +0100

    Add analog host

---
+++ analog-host/vite.config.ts
@@ -0,0 +1,38 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import analog from '@analogjs/platform';
+import { federation } from '@module-federation/vite';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  build: {
+    target: ['es2022'],
+  },
+  resolve: {
+    mainFields: ['module'],
+  },
+  plugins: [
+    analog({
+      ssr: false,
+      static: false,
+      prerender: {
+        routes: [],
+      },
+    }),
+    federation({
+      name: 'host-app',
+      remotes: {
+        remote_app: {
+          type: 'module',
+          name: 'remote_app',
+          entry: 'http://localhost:5174/remoteEntry.js',
+          entryGlobalName: 'remote_app',
+          shareScope: 'default',
+        },
+      },
+      filename: 'remoteEntry.js',
+      shared: [],
+    }),
+  ],
+}));
--- remote-side/vite.config.ts
+++ remote-side/vite.config.ts
@@ -5,7 +5,7 @@ import { federation } from "@module-federation/vite";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   build: {
-    target: "chrome89",
+    target: "es2022",
   },
   resolve: {
     mainFields: ["module"],
